9713910f5f9a97b6cb3e4dc4bd199f23934b8bf6
joe40009 <47844629+joe40009@users.noreply.github.com>
2019-05-27 02:07:24 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 docker-compose up -d
-sleep 2
-docker exec -d mongo mongo --eval "rs.initiate()"
-[ -e /home/joe40009/AIclothes_dockercompose/line_mysql/user_data ] || docker exec -itd mysql bash -c 'mysql -uroot -piii -e "CREATE SCHEMA user_data ;" &&  mysql -uroot -piii -e "CREATE TABLE user_data.line_user (  line_id VARCHAR(50) NOT NULL,  username VARCHAR(50) NOT NULL,  email VARCHAR(50) NULL,  cellphone VARCHAR(10) NULL,  sex VARCHAR(10) NOT NULL,  age VARCHAR(10) NULL,  height VARCHAR(10) NULL,  weight VARCHAR(10) NULL,  time DATETIME NULL,  PRIMARY KEY (line_id));"'
+sleep 5
+docker exec -it mongo mongo --eval 'rs.initiate()'
+sleep 5
+[ -e /home/joe40009/AIclothes_dockercompose/line_mysql ] || docker exec -itd mysql bash -c 'mysql -uroot -piii -e "CREATE SCHEMA user_data ;" &&  mysql -uroot -piii -e "CREATE TABLE user_data.line_user (  line_id VARCHAR(50) NOT NULL,  username VARCHAR(50) NOT NULL,  email VARCHAR(50) NULL,  cellphone VARCHAR(10) NULL,  sex VARCHAR(10) NOT NULL,  age VARCHAR(10) NULL,  height VARCHAR(10) NULL,  weight VARCHAR(10) NULL,  time DATETIME NULL,  PRIMARY KEY (line_id));"'
 sh Activate3KafkaClusters.sh
 sh Activate3HadoopClusters.sh
 sh Activate3SparkClusters.sh
-docker exec -itd master bash -c "cd /root/models ; spark-submit --master spark://master:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 --files modules/clothes5_4310.h5 identify4spark.py"
+sleep 5
+docker exec -it master bash -c "cd /root/models ; spark-submit --master spark://master:7077 --executor-memory 3G --executor-cores 5 --driver-memory 3G --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 --files modules/clothes5_4310.h5 identify4spark.py"
--- stop.sh
+++ stop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker-compose down
-docker run --name mongo -d -v line_mongodb:/data/db mongo mongod
-sleep 2
+sleep 3
+docker run --name mongo -d -v $(pwd)/line_mongodb:/data/db mongo mongod
+sleep 5
 docker exec -it mongo mongo local --eval 'db.system.replset.remove({})'
 docker rm -f mongo
