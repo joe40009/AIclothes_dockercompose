a6bd80ff77638e05d1a6e0188be822627a673b16
joe40009 <47844629+joe40009@users.noreply.github.com>
2019-05-26 22:36:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 docker-compose up -d
 sleep 2
 docker exec -d mongo mongo --eval "rs.initiate()"
-[ -e /home/joe40009/AIclothes_dockercompose/line_mysql ] || docker exec -itd mysql bash -c 'mysql -uroot -piii -e "CREATE SCHEMA user_data ;" &&  mysql -uroot -piii -e "CREATE TABLE user_data.line_user (  line_id VARCHAR(50) NOT NULL,  username VARCHAR(50) NOT NULL,  email VARCHAR(50) NULL,  cellphone VARCHAR(10) NULL,  sex VARCHAR(10) NOT NULL,  age VARCHAR(10) NULL,  height VARCHAR(10) NULL,  weight VARCHAR(10) NULL,  time DATETIME NULL,  PRIMARY KEY (line_id));"'
+[ -e /home/joe40009/AIclothes_dockercompose/line_mysql/user_data ] || docker exec -itd mysql bash -c 'mysql -uroot -piii -e "CREATE SCHEMA user_data ;" &&  mysql -uroot -piii -e "CREATE TABLE user_data.line_user (  line_id VARCHAR(50) NOT NULL,  username VARCHAR(50) NOT NULL,  email VARCHAR(50) NULL,  cellphone VARCHAR(10) NULL,  sex VARCHAR(10) NOT NULL,  age VARCHAR(10) NULL,  height VARCHAR(10) NULL,  weight VARCHAR(10) NULL,  time DATETIME NULL,  PRIMARY KEY (line_id));"'
 sh Activate3KafkaClusters.sh
 sh Activate3HadoopClusters.sh
 sh Activate3SparkClusters.sh
