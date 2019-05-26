e410ab76a570bee4c88514fb2130223eafae024a
joe40009 <47844629+joe40009@users.noreply.github.com>
2019-05-26 23:17:34 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose down
-docker run --name mongo -d -v ./line_mongodb:/data/db mongo mongod
+docker run --name mongo -d -v line_mongodb:/data/db mongo mongod
 sleep 2
 docker exec -it mongo mongo local --eval 'db.system.replset.remove({})'
 docker rm -f mongo
